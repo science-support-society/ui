0949332904b75e4929c0272ae54e946fc3f519b2
pilusonoka <kpilusonoka@evolutiongaming.com>
2017-08-07 20:40:38 +0300

    init template for radio option

---
+++ app/components/RadioOption/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { injectIntl, intlShape } from 'react-intl';
+
+const RadioOption = ({ value, key, message, intl }) => (
+  <div>
+    <input type="radio" id={key} value />
+    <label htmlFor={key}>{message ? intl.formatMessage(message) : value}</label>
+  </div>
+);
+
+RadioOption.propTypes = {
+  value: React.PropTypes.string.isRequired,
+  message: React.PropTypes.object,
+  key: React.PropTypes.number,
+  intl: intlShape.isRequired,
+};
+
+export default injectIntl(RadioOption);
